3e1b2247358350ba0220e873e102b340f2bb2155
Gusti Adli Anshari <54521960+gstdl@users.noreply.github.com>
2023-01-09 19:22:22 +0700

    improve readability

--- python/sorting/insertion_sort.py
+++ python/sorting/insertion_sort.py
@@ -2,7 +2,7 @@ def insertion_sort(arr):
     if len(arr) < 2:
         return arr
     
-    new_arr=[arr[0]]
+    new_arr = [arr[0]]
 
     for i in arr[1:]:
         if i <= new_arr[0]:
@@ -11,10 +11,18 @@ def insertion_sort(arr):
         if i >= new_arr[-1]:
             new_arr.append(i)
             continue
-        for (j, prev_value), next_value in zip(enumerate(new_arr[:-1]), new_arr[1:]):
+        prev_value = new_arr[0]
+        for j, next_value in enumerate(new_arr[1:]):
             if prev_value < i and next_value >= i:
                 new_arr.insert(j+1, i)
                 break
+            prev_value = next_value
+
+        ## the following loop gives the same result but less readable
+        # for (j, prev_value), next_value in zip(enumerate(new_arr[:-1]), new_arr[1:]):
+        #     if prev_value < i and next_value >= i:
+        #         new_arr.insert(j+1, i)
+        #         break
 
     return new_arr
 
