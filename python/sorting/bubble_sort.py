ef77ca51524743e8f19fd1ae48c87b74cba26f9a
Gusti Adli Anshari <54521960+gstdl@users.noreply.github.com>
2023-01-09 21:02:23 +0700

    fix typo & optimize bubble sort

--- python/sorting/bubble_sort.py
+++ python/sorting/bubble_sort.py
@@ -1,8 +1,12 @@
-def buble_sort(arr):
+def bubble_sort(arr):
     for i in range(len(arr)):
+        n_swaps = 0
         for j in range(0, len(arr) - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j + 1], arr[j] = arr[j], arr[j + 1]
+                n_swaps += 1
+        if n_swaps == 0:
+            return arr
     return arr
 
 
@@ -10,7 +14,7 @@ if __name__ == "__main__":
     from time_counter import timeit
 
     arr = [3, 2, 5, 4, 3, 12, 32, -2, 34, -54, 22]
-    sorted_arr = buble_sort(arr)
+    sorted_arr = bubble_sort(arr)
     print(sorted_arr)
 
-    timeit(buble_sort)
+    timeit(bubble_sort)
